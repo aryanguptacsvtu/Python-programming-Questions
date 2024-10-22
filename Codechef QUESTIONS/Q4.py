866378382c27d8ac0eb99a30cfc1fa4d68a26b79
aryanguptacsvtu <148983503+aryanguptacsvtu@users.noreply.github.com>
2024-10-22 21:29:25 +0530

    Q4

---
+++ Codechef QUESTIONS/Q4.py
@@ -0,0 +1,11 @@
+num = int(input())
+
+factorial = 1
+
+while num > 0:
+    factorial *= num
+    num -= 1
+
+print(factorial)
+
+'''Write a program that uses a while loop to find the factorial of a given number.'''
