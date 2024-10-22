1838d6dbf8c5929ea9a55434d63bb478a70939f0
aryanguptacsvtu <148983503+aryanguptacsvtu@users.noreply.github.com>
2024-10-22 21:29:47 +0530

    Q6

---
+++ Codechef QUESTIONS/Q6.py
@@ -0,0 +1,14 @@
+t = int(input())
+for i in range(0,t):
+    a,b = map(int,input().split())
+    print(a+b)
+
+
+'''Add Two Numbers
+Your task is very simple: given two integers A and B
+Write a program to add these two numbers and output the sum.
+
+Input Format
+The first line contains an integer T, the total number of test cases.
+Then follow T lines, each line contains two integers, A and B.
+'''    
