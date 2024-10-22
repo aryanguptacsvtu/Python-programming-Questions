be83a73e6875f51ce26c8eb3adfdb1dd170df61e
aryanguptacsvtu <148983503+aryanguptacsvtu@users.noreply.github.com>
2024-10-22 21:30:23 +0530

    Q8

---
+++ Codechef QUESTIONS/Q8.py
@@ -0,0 +1,18 @@
+t = int(input())
+for i in range(t):
+    s = input()
+    X = s+s
+    print(X)
+
+
+'''
+"String mirror - Double strings":-
+
+Write a program  which does the following:
+
+Accepts the count of test cases -t- in the 1st line.
+First line of each test case consists of a string s.
+
+You need to perform the following operation:
+Create a variable X which contains the string S concatenated with the string S
+Output X for each test case'''
