3315cd5d959bb369bf2bccb7c6b57d5a67ada746
aryanguptacsvtu <148983503+aryanguptacsvtu@users.noreply.github.com>
2024-10-22 21:27:26 +0530

    Q2

---
+++ Codechef QUESTIONS/Q2.py
@@ -0,0 +1,5 @@
+X, Y, Z = map(float,input().split())
+print((X+Y+Z)//3)
+
+
+'''Write a program to print the average score of a student who appeared in three subject's exams and got score X, Y and Z respectively in those subjects.'''
