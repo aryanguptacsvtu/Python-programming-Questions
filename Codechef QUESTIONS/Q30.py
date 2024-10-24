a3c61c0db4a9d054f5e5d52cd266a24541488609
aryanguptacsvtu <148983503+aryanguptacsvtu@users.noreply.github.com>
2024-10-24 18:57:03 +0530

    Q30

---
+++ Codechef QUESTIONS/Q30.py
@@ -0,0 +1,22 @@
+A,B,C,X = map(int,input().split())
+
+if (X==A or X==B or X==C):
+    print("YES")
+    
+else:
+    print("NO")
+
+
+''' "The Three Topics":-
+
+The Chef has reached the finals of the Annual Inter-school Declamation contest.
+For finals, students were asked to prepare 10 topics. However, Chef was only able to prepare three topics, numbered A, B and C — he is totally blank about other topics. This means Chef can only win the contest if he gets the topics A,B or C to speak about.
+On the contest day, Chef gets topic X. Determine whether Chef has any chances of winning the competition.
+
+
+"Input Format":-
+The first and only line of input will contain a single line containing four space-separated integers A,B,C,X — the three topics Chef has prepared and the topic that was given to him on contest day.
+
+"Output Format":-
+For each testcase, output in a single line "Yes" or "No".You may print each character of the string in either uppercase or lowercase (for example, the strings yEs, yes, Yes, and YES will all be treated as identical).
+'''
