20a59147e41a2863a4dcc82f42ef331dada53017
aryanguptacsvtu <148983503+aryanguptacsvtu@users.noreply.github.com>
2024-11-05 10:28:23 +0530

    Q80

---
+++ Codechef QUESTIONS/Q80.py
@@ -0,0 +1,20 @@
+# cook your dish here
+X = int(input())
+
+if(X%4 ==0):
+    print(X+1)
+
+else:
+    print(X-1)
+
+
+'''    "Decrement OR Increment":-
+
+Write a program to obtain a number N and increment its value by 1 if the number is divisible by 4 otherwise decrement its value by 1.
+
+
+"Input Format":-
+First line will contain a number N.
+
+"Output Format":-
+Output a single line, the new value of the number.'''
