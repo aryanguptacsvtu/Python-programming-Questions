8b62976406b4692462f96bf08324b168fd222a2a
aryanguptacsvtu <148983503+aryanguptacsvtu@users.noreply.github.com>
2024-12-20 18:13:54 +0530

    Q114

---
+++ Codechef QUESTIONS/Q114.py
@@ -0,0 +1,32 @@
+# Define the generator function
+def prime_generator(n):
+    count = 0
+    num = 2
+    while count < n:
+        if is_prime(num):
+            yield num
+            count += 1
+        num += 1
+
+# Function to check if a number is prime
+def is_prime(num):
+    if num <= 1:
+        return False
+    if num <= 3:
+        return True
+    if num % 2 == 0 or num % 3 == 0:
+        return False
+    
+    i = 5
+    while i * i <= num:
+        if num % i == 0 or num % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+
+n = int(input())
+prime_gen = prime_generator(n)
+
+for prime_number in prime_gen:  # Print the prime numbers up to the nth term
+    print(prime_number)
