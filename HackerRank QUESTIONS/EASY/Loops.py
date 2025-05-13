d08b4ce586e35e9945c298402d0face10f1d6e67
Aryan Gupta <guptaaryan1578@gmail.com>
2025-05-13 10:24:44 +0530

    Loops

---
+++ HackerRank QUESTIONS/EASY/Loops.py
@@ -0,0 +1,28 @@
+if __name__ == '__main__':
+    n = int(input())
+
+    for i in range(0,n):
+        print(i*i)
+
+
+''' "Loops":-
+
+"Task":-
+The provided code stub reads an integer,n, from STDIN. For all non-negative integers i < n. print i^2.
+
+"Input Format":-
+The first and only line contains the integer,n.
+
+"Output Format":-
+Print n lines, one corresponding to each i .
+
+"Sample Input":- 
+5
+
+"Sample Output":- 
+0
+1
+4
+9
+16
+'''
