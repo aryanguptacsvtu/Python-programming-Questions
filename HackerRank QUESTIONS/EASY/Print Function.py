ea337df79acf8492029c21d962690362a91c3514
Aryan Gupta <guptaaryan1578@gmail.com>
2025-05-13 10:25:20 +0530

    Print Function

---
+++ HackerRank QUESTIONS/EASY/Print Function.py
@@ -0,0 +1,21 @@
+if __name__ == '__main__':
+    n = int(input())
+    
+    for i in range(1,n+1):
+        print(i,end='')
+
+
+''' "Print Function":-
+
+"Input Format":-
+The first line contains an integer,n.
+
+"Output Format":-
+Print the list of integers from 1 through n as a string, without spaces.
+
+"Sample Input":-
+3
+
+"Sample Output":-
+123
+'''
