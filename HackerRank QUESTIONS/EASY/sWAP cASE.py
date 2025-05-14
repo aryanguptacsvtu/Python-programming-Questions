b4c188627e5998f1b4d1d6d0059f5ef45d1ec618
Aryan Gupta <guptaaryan1578@gmail.com>
2025-05-14 09:06:17 +0530

    sWAP cASE

---
+++ HackerRank QUESTIONS/EASY/sWAP cASE.py
@@ -0,0 +1,28 @@
+def swap_case(s):
+    return (s.swapcase())
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
+
+
+''' "Swap Case":-
+
+"Function Description":-
+Complete the swap_case function in the editor below.
+swap_case has the following parameters:
+string s: the string to modify
+
+"Returns":-
+string: the modified string
+
+"Input Format":-
+A single line containing a string,s.
+
+"Sample Input":-
+HackerRank.com presents "Pythonist 2".
+
+"Sample Output":-
+hACKERrANK.COM PRESENTS "pYTHONIST 2".
+'''
